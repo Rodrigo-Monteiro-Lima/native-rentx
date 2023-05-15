40476822b0c0c19915fd5faca38d97f2263b972e
Rodrigo <rodrigoml2395@gmail.com>
2023-05-15 10:17:35 -0300

    style: header content

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,14 +1,18 @@
 import React from 'react'
-import { Container, Header } from './styles'
+import { Container, Header, TotalCars, HeaderContent } from './styles'
 import { StatusBar } from 'react-native'
 import Logo from '../../assets/logo.svg'
+import { RFValue } from 'react-native-responsive-fontsize'
 
 export default function Home() {
   return (
     <Container>
       <StatusBar barStyle="light-content" backgroundColor="transparent" translucent/>
       <Header>
-        <Logo/>
+        <HeaderContent>
+          <Logo width={RFValue(108)} height={RFValue(12)}/>
+          <TotalCars>Total de 12 carros</TotalCars>
+        </HeaderContent>
       </Header>
     </Container>
   )
