654485c6f534a87812132fd96d12ffcbe7f26867
Rodrigo <rodrigoml2395@gmail.com>
2023-05-15 11:46:23 -0300

    feat: accessory

--- src/screens/CarDetails/index.tsx
+++ src/screens/CarDetails/index.tsx
@@ -1,7 +1,15 @@
 import React from 'react'
-import { About, Container, Header, CarImages, Content, Details, Rent, Price, Period, Description, Brand, Name } from './styles'
+import { Accessories, About, Container, Header, CarImages, Content, Details, Rent, Price, Period, Description, Brand, Name } from './styles'
 import BackButton from '../../components/BackButton'
 import ImageSlider from '../../components/ImageSlider'
+import Accessory from '../../components/Accessory'
+import SpeedSvg from '../../assets/speed.svg'
+import AccelerationSvg from '../../assets/acceleration.svg'
+import ForceSvg from '../../assets/force.svg'
+import GasolineSvg from '../../assets/gasoline.svg'
+import ExchangeSvg from '../../assets/exchange.svg'
+import PeopleSvg from '../../assets/people.svg'
+
 
 export default function CarDetails() {
   return (
@@ -22,7 +30,15 @@ export default function CarDetails() {
             <Period>Ao dia</Period>
             <Price>R$ 580</Price>
           </Rent>
-        </Details>  
+        </Details>
+        <Accessories>
+          <Accessory name="380Km/h" icon={SpeedSvg}/>
+          <Accessory name="3.2s" icon={AccelerationSvg}/>
+          <Accessory name="800 HP" icon={ForceSvg}/>
+          <Accessory name="Gasolina" icon={GasolineSvg}/>
+          <Accessory name="Auto" icon={ExchangeSvg}/>
+          <Accessory name="2 pessoas" icon={PeopleSvg}/>
+        </Accessories>
         <About>
           Este é automóvel desportivo. Surgiu do lendário touro de lide indultado na praça Real Maestranza de Sevilla. É um belíssimo carro para quem gosta de acelerar.
         </About>
--- src/screens/CarDetails/styles.ts
+++ src/screens/CarDetails/styles.ts
@@ -74,3 +74,12 @@ export const About = styled.Text`
   margin-top: 23px;
   line-height: ${RFValue(25)}px;
 `;
+
+export const Accessories = styled.View`
+  width: 100%;
+  flex-direction: row;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: space-between;
+  margin-top: 16px;
+`;
