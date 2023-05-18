b326b81520f6d0046c8b209ac4e6c16e9f35794c
Rodrigo <rodrigoml2395@gmail.com>
2023-05-18 10:39:41 -0300

    feat: finishing inputs

--- src/components/PasswordInput/index.tsx
+++ src/components/PasswordInput/index.tsx
@@ -6,23 +6,39 @@ import { TextInputProps } from 'react-native';
 import { BorderlessButton } from 'react-native-gesture-handler';
 
 interface InputProps extends TextInputProps {
-  iconName: React.ComponentProps<typeof Feather>['name']
+  iconName: React.ComponentProps<typeof Feather>['name'];
+  value?: string;
 }
 
-export default function PasswordInput({ iconName, ...rest }: InputProps) {
+export default function PasswordInput({ iconName, value, ...rest }: InputProps) {
   const theme = useTheme();
   const [isPasswordVisible, setIsPasswordVisible] = useState(true);
+  const [isFocused, setIsFocused] = useState(false);
+  const [isFilled, setIsFilled] = useState(false);
+  function handleInputFocus() {
+    setIsFocused(true);
+  }
+  function handleInputBlur() {
+    setIsFocused(false);
+    setIsFilled(!!value);
+  }
   function handlePasswordVisibilityChange() {
     setIsPasswordVisible(prevState => !prevState);
   }
   return (
-    <Container>
+    <Container isFocused={isFocused}>
       <IconContainer>
-        <Feather name={iconName} size={24} color={theme.colors.text_detail} />
+        <Feather 
+          name={iconName} 
+          size={24} 
+          color={(isFocused || isFilled) ? theme.colors.main : theme.colors.text_detail} 
+        />
       </IconContainer>        
       <InputText 
         {...rest}
         secureTextEntry={isPasswordVisible}
+        onFocus={handleInputFocus}
+        onBlur={handleInputBlur}
       />
       <BorderlessButton onPress={handlePasswordVisibilityChange}>
         <IconContainer>
--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,41 +1,54 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Form, Container, Header, SubTitle, Title, Footer } from './styles'
-import { StatusBar } from 'react-native'
+import { Keyboard, KeyboardAvoidingView, StatusBar } from 'react-native'
 import Button from '../../components/Button'
 import { useTheme } from 'styled-components'
 import Input from '../../components/Input'
 import PasswordInput from '../../components/PasswordInput'
-import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
+import { TouchableWithoutFeedback } from 'react-native-gesture-handler'
 
 export default function SignIn() {
   const theme = useTheme();
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
   return (
-    <Container>
-      <StatusBar barStyle="dark-content" backgroundColor="transparent" translucent/>
-      <Header>
-        <Title>Estamos{'\n'} quase lá</Title>
-        <SubTitle>
-          Faça seu login para começar{'\n'}
-          uma experiência incrível.
-        </SubTitle>
-      </Header>
-      <Form>
-        <Input 
-          iconName='mail'
-          placeholder='E-mail'
-          keyboardType='email-address'
-          autoCorrect={false}
-          autoCapitalize='none'
-        />
-        <PasswordInput
-          iconName='lock'
-          placeholder='Senha'
-        />
-      </Form>
-      <Footer>
-        <Button title="Login" onPress={() => {}} enabled={false} loading={false}/>
-        <Button title="Criar conta gratuita" onPress={() => {}} enabled={true} loading={false} color={theme.colors.background_secondary} light/> 
-      </Footer>
-    </Container>
+    <KeyboardAvoidingView
+      behavior='position'
+      enabled
+    >
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <Container>
+          <StatusBar barStyle="dark-content" backgroundColor="transparent" translucent/>
+          <Header>
+            <Title>Estamos{'\n'} quase lá</Title>
+            <SubTitle>
+              Faça seu login para começar{'\n'}
+              uma experiência incrível.
+            </SubTitle>
+          </Header>
+          <Form>
+            <Input 
+              iconName='mail'
+              placeholder='E-mail'
+              keyboardType='email-address'
+              autoCorrect={false}
+              autoCapitalize='none'
+              onChangeText={setEmail}
+              value={email}
+            />
+            <PasswordInput
+              iconName='lock'
+              placeholder='Senha'
+              onChangeText={setPassword}
+              value={password}
+            />
+          </Form>
+          <Footer>
+            <Button title="Login" onPress={() => {}} enabled={false} loading={false}/>
+            <Button title="Criar conta gratuita" onPress={() => {}} enabled={true} loading={false} color={theme.colors.background_secondary} light/> 
+          </Footer>
+        </Container>
+      </TouchableWithoutFeedback>
+    </KeyboardAvoidingView>
   )
 }
